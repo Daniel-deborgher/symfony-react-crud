f9281872d1f3a71bb383cc1c306465ee7c01dce8
Daniel Deborgher <daniel.deborgher@gmail.com>
2022-01-19 11:21:13 +0100

    first commit

--- assets/pages/VoitureShow.js
+++ assets/pages/VoitureShow.js
@@ -19,7 +19,7 @@ function VoitureShow() {
     return (
         <Layout>
            <div className="container">
-            <h2 className="text-center mt-5 mb-3">Voiture</h2>
+            <h2 className="text-center mt-5 mb-3">Voiture : {voiture.model}</h2>
                 <div className="card">
                     <div className="card-header">
                         <Link 
